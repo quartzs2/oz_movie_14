6f106a00ccfce4d1ae8693421ff4580f9ebea799
quartzs2 <csh001231@gmail.com>
2025-11-06 14:02:17 +0900

    feat: react-router 설정(#1)
    - react-router 설치
    - home, 404 페이지 라우팅
    - MainLayout 생성 및 설정
    - ROUTES_PATH 상수 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,29 @@
+import { Routes, Route } from "react-router";
+import MainLayout from "./components/layout/MainLayout";
+import { Home, NotFound } from "./pages";
+import { ROUTE_PATHS } from "./constants/urls";
+
 function App() {
-  return <></>;
+  const ROUTES = [
+    {
+      path: ROUTE_PATHS.HOME,
+      element: <Home />,
+    },
+    {
+      path: ROUTE_PATHS.NOT_FOUND,
+      element: <NotFound />,
+    },
+  ];
+
+  return (
+    <Routes>
+      <Route element={<MainLayout />}>
+        {ROUTES.map((route) => (
+          <Route key={route.path} path={route.path} element={route.element} />
+        ))}
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
