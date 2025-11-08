b6231925d1c29adeb58c57fc141eede3fbcbe8ca
quartzs2 <csh001231@gmail.com>
2025-11-09 00:13:49 +0900

    feat: 캐러셀 구현(#13)
    - 캐러셀 공통 컴포넌트 구현
    - fetchNowPlayingMovies query 구현
    - 메인 페이지 html 구조 수정 및 캐러셀 추가

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,5 @@ export { default as Header } from "./layout/Header";
 export { default as MainLayout } from "./layout/MainLayout";
 export { default as SearchBar } from "./layout/SearchBar";
 export { default as MovieCard } from "./movie/MovieCard";
+export { default as NowPlayingCarousel } from "./movie/NowPlayingCarousel";
+export { default as Carousel } from "./ui/Carousel";
